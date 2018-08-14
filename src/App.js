240b2bbffbc2af4bf5fe11f0a4c3d18fd3f43e7c
nachumfreedman <nochum09@gmail.com>
2018-08-14 18:22:15 +0300

    header update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Carousel from './Carousel'
+import Carousel from './Carousel';
+import Header from './header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Carousel />
+        <Header/>
       </div>
     );
   }
--- src/header.js
+++ src/header.js
@@ -1,39 +1,16 @@
 import React from 'react';
-import Slider from 'react-slick';
-import typewriter from './typewriter.jpg';
-import record from './record.jpg';
-import train from './train.jpg';
-import feynman from './feynman.png';
-import './Carousel.css';
+import './header.css';
 
-class Carousel extends React.Component {
-  render() {
-    var settings = {
-      dots: true,
-      infinite: true,
-      speed: 100,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      className: 'carousel',
-    };
-    
+class Header extends React.Component {
+  render() {    
     return (
-      <Slider {...settings}>
-          <div>
-            <img src={typewriter} alt='typewriter'/>
-          </div>
-          <div>
-            <img src={record} alt='record'/>
-          </div>        
-          <div>
-            <img src={feynman} alt='Dr Richard Feynman'/>
-          </div>
-          <div>
-            <img src={train} alt='train'/>
-          </div>
-      </Slider>
+      <div className='header-container'>
+        <button>WORK</button>
+        <button>ABOUT</button>
+        <button>CONTACT</button>
+      </div>
     );
   }
 }
 
-export default Carousel;
+export default Header;
